bb8192d86b3e89b8b1c140839ac74133c423c695
nehakerung <nehakerung@gmail.com>
2024-10-02 13:55:22 +0100

    Set up site menu for linking to the homepage and other pages

--- base/templatetags/navigation_tags.py
+++ base/templatetags/navigation_tags.py
@@ -1,9 +1,8 @@
 from django import template
-
+from wagtail.models import Site
 from base.models import FooterText
 
 register = template.Library()
-
 
 @register.inclusion_tag("base/includes/footer_text.html", takes_context=True)
 def get_footer_text(context):
@@ -16,3 +15,7 @@ def get_footer_text(context):
     return {
         "footer_text": footer_text,
     }
+
+@register.simple_tag(takes_context=True)
+def get_site_root(context):
+    return Site.find_for_request(context["request"]).root_page
